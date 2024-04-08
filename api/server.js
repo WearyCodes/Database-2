d57ba9b383cd178ebb26ce73018506ea1e4cf2fc
Samuel Sanchez <wearycodes@gmail.com>
2024-04-07 20:37:52 -0500

    Triumph of cars seed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
 const server = express()
-
+console.log('YOU HAVE THE RIGHT STUFF')
 // DO YOUR MAGIC
 
 module.exports = server
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'Chrysler',
+        model: '200',
+        mileage: 123000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '4444444444444',
+        make: 'Toyota',
+        model: 'Corola',
+        mileage: 123123,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '3333333333333',
+        make: 'Chrysler',
+        model: '200',
+        mileage: 123000,
+    }
+]
+
+exports.seed = async function(knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}

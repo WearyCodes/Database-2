0a5f2ae10e6a00d61459d19b5941b62d1f38d438
Samuel Sanchez <wearycodes@gmail.com>
2024-04-08 20:32:34 -0500

    Cars get working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 console.log('YOU HAVE THE RIGHT STUFF')
 // DO YOUR MAGIC
 
 server.use(express.json())
+
+server.use('/api/cars', carsRouter)
 
 server.use('*', (req, res, next) => {
     next({status: 404, message: 'Page not found'})
